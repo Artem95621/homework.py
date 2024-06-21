586952ac88e44b307d0f4702f5835136f7bdf06b
Artem95621 <hachatryan1995@mail.ru>
2024-06-21 11:12:42 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+The_number_of_completed_HW = 12
+Quantity_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = ( Quantity_of_hours_spent / The_number_of_completed_HW )
+print( 'курс:', Course_name , 'всего задач:', The_number_of_completed_HW , 'затрачено часов:', Quantity_of_hours_spent , 'среднее время выполнения', Time_for_one_task)
