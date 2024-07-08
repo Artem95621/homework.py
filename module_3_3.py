24903b95a70d7fe0fc5c55c70b4544470a4e0358
Artem95621 <hachatryan1995@mail.ru>
2024-07-08 18:47:02 +0500

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,11 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+values_list = [2, 'Oleg', 867.21]
+values_dict = {'a': 23, 'b': 'Lego', 'c': 87.1}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [67, 'greece']
+print_params(*values_list_2, 42)
