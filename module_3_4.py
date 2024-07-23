6fc12a089dee8a8aaa3cdd2b273f3a498daf2dc5
Artem95621 <hachatryan1995@mail.ru>
2024-07-23 12:45:32 +0500

    module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,4 +1,14 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in range(len(other_words)):
-        
+                if other_words[i].lower() in root_words.lower():
+            same_words.append(other_words[i])
+        elif root_words.lower() in other_words[i].lower():
+            same_words.append(other_words[i])
+
+    return same_words
+
+same_words_1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+same_words_2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(same_words_1)
+print(same_words_2)
